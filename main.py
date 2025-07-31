f07460b949cf71273c16c5d0379e33ab7fd6702c
michal <michalkowalski876@gmail.com>
2025-07-31 20:54:42 +0200

    pesel_gen started

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
 import json
 import random
 
+
 def get_names():
     with open('names.json', 'r') as fh:
         content = fh.read()
         names = json.loads(content)
-        return names
 
-def random_person(names):
-    gender = random.choice(['male', 'female'])
+    return names
 
 
+def random_person(names, multiple):
+    iterations = 1
+    people = []
 
-def mult_people():
-    pass
+    if multiple:
+        iterations = int(input('How many people? \n'))
+
+    for i in range(iterations):
+        person = {}
+        gender = random.choice(['male', 'female'])
+        person['gender'] = gender
+        if gender == 'male':
+            person['name'] = random.choice(names['male_names'])
+            person['surname'] = random.choice(names['male_surnames'])
+        else:
+            person['name'] = random.choice(names['female_names'])
+            person['surname'] = random.choice(names['female_surnames'])
+
+        people.append(person)
+        print(people)
+
+    return people
+
 
 def custom_person():
     pass
 
+def pesel_gen():
+    pesel = ''
+    date_of_birth = input('year of birth(YYYY-MM-DD): ')
+    gender = input('gender(F/M): ')
+    date_of_birth = date_of_birth.split('-')
+    print(date_of_birth)
 
 
 def menu():
-    while(True):
+    while (True):
         action = input('choose action: \n'
                        '1. random person \n'
                        '2. multiple random people \n'
@@ -29,15 +54,16 @@ def menu():
                        '4. stop \n')
 
         if action == '1':
-            random_person(get_names)
-        if action == '2':
+            random_person(get_names(), False)
+        elif action == '2':
+            random_person(get_names(), True)
+        elif action == '3':
             custom_person()
-        if action == '3':
-            custom_person()
-        if action == '4':
-            break
+        elif action == '5':
+            pesel_gen()
         else:
             print('invalid')
+
 
 if __name__ == '__main__':
     menu()
